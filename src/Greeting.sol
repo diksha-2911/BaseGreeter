86cd947d3109b27f66a6407adf94d54f62e40fb0
Diksha <dikshas2911@gmail.com>
2025-10-15 18:25:31 +0530

    added base greeter

---
+++ src/Greeting.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Greeter {
+    string greeting;
+    constructor(string memory _greeting){
+        greeting = _greeting;
+    }
+    function setGreeting(string memory newGreeting) public{
+        greeting = newGreeting;
+    }
+    function greeter() public view returns(string memory){
+        return (greeting);
+    }
+}
